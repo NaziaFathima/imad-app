f97127909a1f0772bc2af3a940fa5a3c4241215e
NaziaFathima <nfathima07@gmail.com>
2017-08-19 04:55:49 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,28 @@
 console.log('Loaded!');
 
 // Change the text of the main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-
-//move image
-var img  = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval= setInterval(moveRight,50);
-  
-};
+ var button = document.getElementById('counter');
+ 
+ button.onCLick = function(){
+     
+     //create a request to the counter endpoint
+     var request = new XMLHttpRequest();
+     
+     
+     //capture the response and store it in a variable
+     request.onreadystatechange = function(){
+         if(request.readyState == XMLHttpRequest.Done){
+             if(request.status == 200){
+                var counter =  request.responeText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+             }
+         } 
+     };
+     
+    //Make the request
+    request.open('GET','http://nfathima07.imad.hasura-app.io/counter', true);
+    request.send(null);
+ 
+     
+ }
