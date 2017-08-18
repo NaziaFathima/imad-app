ad84959d639a159b6a0c8a0ea6a348c8b836b3cd
NaziaFathima <nfathima07@gmail.com>
2017-08-19 04:04:55 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 // Change the text of the main-text div
 var element = document.getElementById('main-text');
-element.InnerHTML = "New Value";
+element.InnerHTML = 'New Value';
 
 //move image
 var img  = document.getElementById('madi');
