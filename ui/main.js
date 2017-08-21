8babd8c3d695c7b31e91d0f622ab518a99c15700
NaziaFathima <nfathima07@gmail.com>
2017-08-22 02:53:08 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,7 +59,7 @@
      
      //Make a request to the server and send the name
      
-     request.open('GET','http://nfathima07.imad.hasura-app.io/submit-name?name='+ name, true);
+    request.open('GET','http://nfathima07.imad.hasura-app.io/submit-name?name='+ name, true);
     request.send(null);
     
  };
