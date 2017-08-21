c138ad50d609246a4a426555396f5bdc8524edf2
NaziaFathima <nfathima07@gmail.com>
2017-08-22 01:23:20 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,10 +28,38 @@
  var nameInput = document.getElementById('name');
  var name1 = nameInput.value;
  var submit = document.getElementById('submit_btn');
- submit.onclick = function() {
+ submit.onclick = function()
+ {
+     
+     //create a request object
+     var request = new XMLHttpRequest();
+     
+     
+     //capture the response and store it in a variable
+     request.onreadystatechange = function()
+    {
+         if(request.readyState === XMLHttpRequest.Done)
+        {
+            if(request.status === 200)
+            {
+                var names = ['name1','name2','name3','name4'];
+                var list = '';
+                for(var i=0; i<names.length;i++){
+                list +='<li>' + names[i] + '</li>';
+         
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+        }
+    } 
+};
+     
+     
      //Make a request to the server and send the name
      
-     
+     request.open('GET','http://nfathima07.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
      //Capture a list of names and render it as a list
      var names = ['name1','name2','name3','name4'];
      var list = '';
