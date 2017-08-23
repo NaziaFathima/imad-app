4776f5c18249be864658b34ffccbb31b5e7ccbcd
NaziaFathima <nfathima07@gmail.com>
2017-08-24 04:36:32 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var pool = require('path');
+var config = { 
+    user: 'nfathima07',
+    database: 'nfathima07',
+    host: 'db.imad.hasura.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
+
+
 var app = express();
 app.use(morgan('combined'));
 var articleOne={
@@ -54,6 +64,19 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a select request
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+           res.send(JSON.stringify(result)); 
+        }
+    })
 });
 
 var counter = 0;
