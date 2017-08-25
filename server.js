40857886adc22f18c3603a88ba06582a511418b9
NaziaFathima <nfathima07@gmail.com>
2017-08-26 01:50:04 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,23 @@ app.get('/counter',function(req,res){
 });
 
 
+app.get('/articles/:articleName', function(req,res){
+    
+    pool.query('SELECT * FROM article WHERE title =' + req.params.articleName, function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else {
+           if(result.rows.length === 0){
+               res.status(404).send('Article not found');
+           } else{
+               var articleData  = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+        
+    });
+    res.send(createTemplate(articles[articleName]));
+});
 app.get('/article-one', function (req, res) {
   res.send(createTemplate(articleOne));
 });
